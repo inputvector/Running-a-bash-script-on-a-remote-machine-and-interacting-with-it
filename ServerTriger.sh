c6afaaff3dde4ff808df67343f481ddc9e041f06
Bekir Dabanoglu <65566906+inputvector@users.noreply.github.com>
2021-11-10 15:30:46 -0800

    Add files via upload

---
+++ ServerTriger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+N1=$(shuf -i 1-1000 -n 1)
+N2=$(shuf -i 1-1000 -n 1)
+ans=$(shuf -i 1-4 -n 1)
+
+if [ "$ans" -eq 1 ]
+then
+  operation="+"
+elif [ "$ans" -eq 2 ]
+then
+  operation="\-"
+elif [ "$ans" -eq 3 ]
+then
+  operation="\*"
+else
+  operation="/"
+fi
+
+expect runScriptOnServer.exp $N1 $N2 "$operation"
